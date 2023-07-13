536b2cae3831dd8de3900f1507fab10a0f1914fd
Randy Beltran <131041473+Beltranr20@users.noreply.github.com>
2023-07-13 01:01:41 +0000

    recent changes that fixed random generator

--- src/app.js
+++ src/app.js
@@ -6,32 +6,41 @@ import "./style.css";
 // import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  document.querySelector(".card").classList.add(generateRandomSuit());
-};
+  var suitRandom = generateRandomSuit();
 
-let generateRandomNumber = () => {
-  let numbers = [
-    "A",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let indexNumbers = Math.floor(Math.random() * numbers.length);
-  return numbers[indexNumbers];
-};
+  let suitTop = document.querySelector(".top-suit");
+  suitTop.innerHTML = suitRandom;
 
-let generateRandomSuit = () => {
-  let suit = ["clubs", "diamonds", "hearts", "spades"];
-  let indexSuit = Math.floor(Math.random() * suit.length);
-  return suit[indexSuit];
+  let suitBottom = document.querySelector(".bottom-suit");
+  suitBottom.innerHTML = suitRandom;
+
+  let suitMiddle = document.querySelector(".numbers");
+  suitMiddle.innerHTML = generateRandomNumber();
+
+  function generateRandomNumber() {
+    let numbers = [
+      "A",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumbers];
+  }
+
+  function generateRandomSuit() {
+    let suit = ["♣", "♦", "♥", "♠"];
+    let indexSuit = Math.floor(Math.random() * suit.length);
+    return suit[indexSuit];
+  }
 };
